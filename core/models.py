fe2d3a3d0cf3257de5d7cb069d0dc4fa62207a0b
david <david.roberson2010@gmail.com>
2024-04-16 17:28:45 -0300

    criando os models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+import uuid
+from stdimage.models import StdImageField
 
-# Create your models here.
+def get_file_path(_instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return filename
+
+class Base(models.Model):
+    criados = models.DateField('Criação', auto_now_add=True)
+    modificado = models.DateField('Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+class People(Base):
+    CHOICES = (
+        ('convidado', 'convidado'),
+        ('padrinho','padrinho'),
+        ('noivo', 'noivo'),
+        ('noiva', 'noiva'),
+    )
+    nome = models.CharField('Nome', max_length=100)
+    tipo = models.CharField('Tipo', max_length=9, choices=CHOICES)
+    imagem = StdImageField('Imagem', upload_to=get_file_path,
+                           variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
+    facebook = models.CharField('Facebook', max_length=100, default='#')
+    twitter = models.CharField('Twitter', max_length=100, default='#')
+    instagram = models.CharField('Instagram', max_length=100, default='#')
+
+    class Meta:
+        verbose_name = 'Pessoa'
+        verbose_name_plural = 'Pessoas'
+
+    def __str__(self):
+        return self.nome
--- core/views.py
+++ core/views.py
@@ -1,4 +1,10 @@
 from django.views.generic import TemplateView
+from .models import People
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['people'] = People.objects.order_by('?').all()
+        return context
