ed1a2e7eda559816e2b95ee2b80f8b42140e7f71
David Roberson <david.roberson2010@gmail.com>
2024-05-19 14:51:09 -0300

    oidddjfoi

--- core/admin.py
+++ core/admin.py
@@ -5,15 +5,20 @@ from .models import Padrinho, Madrinha, Noiva, Noivo
 @admin.register(Padrinho)
 class PadrinhoAdmin(admin.ModelAdmin):
     list_display = ('nome',)
+    exclude = ('noivo',)
+
 
 @admin.register(Madrinha)
 class MadrinhaAdmin(admin.ModelAdmin):
     list_display = ('nome',)
 
+
 @admin.register(Noivo)
 class NoivoAdmin(admin.ModelAdmin):
     list_display = ('nome',)
 
+
 @admin.register(Noiva)
 class NoivaAdmin(admin.ModelAdmin):
-    list_display = ('nome', )
+    list_display = ('nome',)
+
