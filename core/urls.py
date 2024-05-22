f4f8d69d0bbc62e69a3443a934dc8f697e31a63e
David Roberson <david.roberson2010@gmail.com>
2024-05-22 19:50:02 -0300

    oidddjfoi

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
-from django.views.generic import TemplateView
-
-from . views import IndexView, rsvp_view
-
+from core.views import IndexView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('rsvp/', rsvp_view, name='rsvp'),
-    path('rsvp/thanks/', TemplateView.as_view(template_name="thanks.html"), name='rsvp_thanks'),
 ]
