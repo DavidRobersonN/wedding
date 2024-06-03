9eb9bc3f01105d0906a88f15dcbf77e3abab4db9
David Roberson <david.roberson2010@gmail.com>
2024-06-03 19:35:08 -0300

    kjhkjh

--- wedding/settings.py
+++ wedding/settings.py
@@ -83,9 +83,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en_US'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTF-8'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
