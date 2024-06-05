f1326626623b8935662b7ff45d7ba058393910e5
David Roberson <david.roberson2010@gmail.com>
2024-06-05 19:34:29 -0300

    kjhklujh

---
+++ wedding/settings_dev.py
@@ -0,0 +1,6 @@
+from .settings import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+# Qualquer outra configuração específica de desenvolvimento
---
+++ wedding/settings_prod.py
@@ -0,0 +1,6 @@
+from .settings import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['wedding-production-9b77.up.railway.app']
+
+# Qualquer outra configuração específica de produção
