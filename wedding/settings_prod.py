85e7e95ed3a427e534235b96ec863eace395c344
David Roberson <david.roberson2010@gmail.com>
2024-06-09 11:59:55 -0300

    joilj

--- wedding/settings_prod.py
+++ wedding/settings_prod.py
@@ -2,8 +2,8 @@ from .settings import *
 import dj_database_url
 
 DEBUG = False
-ALLOWED_HOSTS = ['wedding-production-9b77.up.railway.app']
+ALLOWED_HOSTS = ['wedding-production-9b77.up.railway.app', 'wedding-production-9b77.up.railway.app/admin']
 # Database
 DATABASES = {'default': dj_database_url.config(default=os.environ.get('DJANGO_DATABASE_URL'))}
 # Qualquer outra configuração específica de produção
-CSRF_TRUSTED_ORIGINS = ['wedding-production-9b77.up.railway.app']  # Adicione seu domínio Railway aqui
+CSRF_TRUSTED_ORIGINS = ['https://wedding-production-9b77.up.railway.app', 'https://wedding-production-9b77.up.railway.app/admin']  # Adicione seu domínio Railway aqui
