82d4fcf79e68cc3e067a658c34253a7359fec78b
David Roberson <david.roberson2010@gmail.com>
2024-06-09 11:43:43 -0300

    joilj

--- wedding/settings_prod.py
+++ wedding/settings_prod.py
@@ -2,7 +2,7 @@ from .settings import *
 import dj_database_url
 
 DEBUG = False
-ALLOWED_HOSTS = ['wedding-production-9b77.up.railway.app']
+ALLOWED_HOSTS = ['wedding-production-9b77.up.railway.app', 'wedding-production-9b77.up.railway.app/admin']
 # Database
 DATABASES = {'default': dj_database_url.config(default=os.environ.get('DJANGO_DATABASE_URL'))}
 # Qualquer outra configuração específica de produção
