02e87484b731582efbd101e846312dae0153cfcf
David Roberson <david.roberson2010@gmail.com>
2024-06-16 19:47:53 -0300

    joilj

--- wedding/settings.py
+++ wedding/settings.py
@@ -104,5 +104,5 @@ SOCIAL_AUTH_RAISE_EXCEPTIONS = False
 # configurações para Facebook
 SOCIAL_AUTH_FACEBOOK_KEY = '7882417811853822'
 SOCIAL_AUTH_FACEBOOK_SECRET = '4bb874e4cf41f610bb2737d06d0530b1'
-SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+#SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 SOCIAL_AUTH_FACEBOOK_REDIRECT_URI = 'https://wedding-production-9b77.up.railway.app/social-auth/complete/facebook/'
